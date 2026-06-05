66a717ae143020cf7920be49d8f3625e7f7527cd
srushti628 <srushtibasavaraj13@gmail.com>
2026-06-05 09:06:38 +0530

    Create canvas with dimensions and print message
    Initialize a canvas with predefined dimensions and print a message.

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+#define WIDTH 80
+#define HEIGHT 24
+
+#define EMPTY '_'
+
+char picture[HEIGHT][WIDTH];
+
+int main() {
+    printf("Canvas created of size %d x %d\n", WIDTH, HEIGHT);
+    return 0;
+}
