1ae74d2aa268c3002003fd7f837f4b4979585e7f
srushti628 <srushtibasavaraj13@gmail.com>
2026-06-06 20:55:40 +0530

    Implement picture manipulation functions
    Added functions to clear and display a picture, and set a pixel.

--- main.c
+++ main.c
@@ -1,13 +1,43 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 #define WIDTH 80
 #define HEIGHT 24
 
 #define EMPTY '_'
+#define PIXEL '*'
+
 
 char picture[HEIGHT][WIDTH];
+void clearPicture()
+{
+    int i,j;
+    for(i=0;i<HEIGHT;i++)
+        {
+            for(j=0;j<WIDTH;j++)
+                {
+                    picture[i][j]=EMPTY;
 
-int main() {
-    printf("Canvas created of size %d x %d\n", WIDTH, HEIGHT);
-    return 0;
 }
+        }
+}
+void displayPicture()
+{
+    int i,j;
+for(i=0;i<HEIGHT;i++)
+{
+for(j=0;j<WIDTH;j++)
+{
+printf("%c", picture[i][j]);
+}
+printf("\n");
+}
+}
+void setPixel(int x, int y)
+{
+    if(x>=0 && x<WIDTH && y>=0 && y<HEIGHT)
+{
+picture[y][x]=PIXEL;
+}
+}
+
